68a11111637ec4e4d2b4413822af4e87e07cb777
Nick Mazzotta <nickamazzotta@gmail.com>
2018-01-06 05:44:38 -0500

    implemented simple animation in 3 ways

---
+++ src/starAnimation/utilityTimer/UtilityTimerExample.java
@@ -0,0 +1,34 @@
+package starAnimation.utilityTimer;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UtilityTimerExample extends JFrame {
+	
+	public UtilityTimerExample() {
+		initUI();
+	}
+	
+	private void initUI() {
+		
+		add(new Board());
+		
+		setResizable(false);
+		pack();
+		
+		setTitle("Star");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame ex  = new UtilityTimerExample();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
