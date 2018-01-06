0631e6bcd2dd2f75c41c45c6725659e8c740e73c
NickMazzotta <nickamazzotta@gmail.com>
2018-01-06 00:42:27 -0500

    begin swing timer implementation of animation

---
+++ src/starAnimation/swingTimer/SwingTimerExample.java
@@ -0,0 +1,35 @@
+package starAnimation.swingTimer;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class SwingTimerExample extends JFrame {
+
+	public SwingTimerExample() {
+		initUI();
+	}
+	
+	private void initUI() {
+		
+		add(new Board());
+		
+		setResizable(false);
+		pack();
+		
+		setTitle("Star");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame ex = new SwingTimerExample();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
