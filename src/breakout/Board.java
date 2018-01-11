4c00d7f3788603422a748e6742f84566b384bc68
Nick Mazzotta <nickamazzotta@gmail.com>
2018-01-10 21:00:24 -0500

    beginning breakout implementation

---
+++ src/breakout/Board.java
@@ -0,0 +1,83 @@
+package breakout;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.util.Timer;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel implements Commons {
+	
+	private Timer timer;
+	private String message = "Game Over";
+	private Ball ball;
+	private Paddle paddle;
+	private Brick bricks[];
+	private boolean ingame = true;
+	
+	public Board() {
+		
+		initBoard();
+	}
+	
+	private void initBoard() {
+		
+		addKeyListener(new TAdapter());
+		setFocusable(true);
+		
+		bricks = new Brick[N_OF_BRICKS];
+		setDoubleBuffered(true);
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new ScheduleTask(), DELAY, PERIOD);
+	}
+	
+	@Override
+	public void addNotify() {
+		
+		super.addNotify();
+		gameInit();
+	}
+	
+	private void gameInit() {
+		
+		ball = new Ball();
+		paddle = new Paddle();
+		
+		int k = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 6; j++) {
+				bricks[k] = new Brick(j * 40 + 30, i * 10 + 50);
+				k++;
+			}
+		}
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+		
+		if (ingame) {
+			
+			drawObjects(g2d);
+		} else {
+			
+			gameFinished(g2d);
+		}
+		
+		Toolkit.getDefaultToolkit().sync();
+	}
+	
+	private void drawObjects(Graphics2D g2d) {
+		
+	}
+}
