76f2c62ef26bf22e7bca499f291f2d9b00c80bbb
NickMazzotta <nickamazzotta@gmail.com>
2018-04-11 22:14:02 -0400

    added variables and constants

---
+++ src/pacman/Board.java
@@ -0,0 +1,52 @@
+package pacman;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Event;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+
+	private Dimension d;
+	private final Font smallFont = new Font("Helvetica", Font.BOLD, 14);
+	
+	private Image ii;
+	private final Color dotColor = new Color(192, 192, 0);
+	private Color mazeColor;
+	
+	private boolean inGame = false;
+	private boolean dying = false;
+	
+	private final int BLOCK_SIZE = 24;
+	private final int N_BLOCKS = 15;
+	private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+	private final int PAC_ANIM_DELAY = 2;
+	private final int PACMAN_ANIM_COUNT = 4;
+	private final int MAX_GHOSTS = 12;
+	private final int PACMAN_SPEED = 6;
+	
+	private int pacAnimCount = PAC_ANIM_DELAY;
+	private int pacAnimdir = 1;
+	private int pacmanAnimPos = 0;
+	private int N_GHOSTS = 6;
+	private int pacsLeft, score;
+	private int [] dx, dy;
+	private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
+	
+	private Image ghost;
+	private Image pacman1, pacman2up, pacman2left, pacman2right, pacman2down;
+}
