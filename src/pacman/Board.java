30eaadcd936532d9b7831dd023d32ecd8c747a6f
NickMazzotta <nickamazzotta@gmail.com>
2018-04-17 00:29:42 -0400

    added all variables and constants

--- src/pacman/Board.java
+++ src/pacman/Board.java
@@ -49,4 +49,47 @@ public class Board extends JPanel implements ActionListener {
 	
 	private Image ghost;
 	private Image pacman1, pacman2up, pacman2left, pacman2right, pacman2down;
+	private Image pacman3up, pacman3down, pacman3left, pacman3right;
+	private Image pacman4up, pacman4down, pacman4left, pacman4right;
+	
+	private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+	private int req_dx, req_dy, view_dx, view_dy;
+	
+	private final short levelData[] = {
+		19, 26, 26, 26, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
+        21, 0, 0, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        21, 0, 0, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        21, 0, 0, 0, 17, 16, 16, 24, 16, 16, 16, 16, 16, 16, 20,
+        17, 18, 18, 18, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 16, 24, 20,
+        25, 16, 16, 16, 24, 24, 28, 0, 25, 24, 24, 16, 20, 0, 21,
+        1, 17, 16, 20, 0, 0, 0, 0, 0, 0, 0, 17, 20, 0, 21,
+        1, 17, 16, 16, 18, 18, 22, 0, 19, 18, 18, 16, 20, 0, 21,
+        1, 17, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 20, 0, 21,
+        1, 17, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 20, 0, 21,
+        1, 17, 16, 16, 16, 16, 16, 18, 16, 16, 16, 16, 20, 0, 21,
+        1, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0, 21,
+        1, 25, 24, 24, 24, 24, 24, 24, 24, 24, 16, 16, 16, 18, 20,
+        9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 25, 24, 24, 24, 28
+	};
+	
+	private final int validSpeeds[] = {1, 2, 3, 4, 6, 8};
+	private final int maxSpeed = 6;
+	
+	private int currentSpeed = 3;
+	private short[] screenData;
+	private Timer timer;
+	
+	public Board() {
+		
+		loadImages();
+		initVariables();
+		initBoard();
+	}
+	
+	private void initBoard() {
+		addKeyListener(new TAdapter());
+		
+	}
 }
+
